2688fedd26ee662fa6b9e745c88e5f5d9323bb36
dharamghevariya <dharamghevariya36@gmail.com>
2025-08-17 17:12:42 -0400

    dividing all the three channels of colors of the image

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ import numpy as np
 
 img = cv2.imread("img/strawberry.jpeg")
 
-img_grap = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img_grap = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# print(img_grap.shape)
+
+imgBlue = img[:,:,0]
+imgGreen = img[:,:,1]
+imgRed = img[:,:,2]
+
+new_img = np.hstack((imgBlue, imgGreen, imgRed))
 
 
-cv2.imshow("Original", img_grap)
+cv2.imshow("Original", new_img)
+
 
 cv2.waitKey(0)
